4ed17c51416bbeff20bf32a0f0e595be649978bf
Uzair Iqbal <uzair.qq@outlook.com>
2018-01-30 00:38:50 +0500

    created employee component and under it created two more components called employee and employee-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
-
-
-
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+
   ],
   imports: [
     BrowserModule
