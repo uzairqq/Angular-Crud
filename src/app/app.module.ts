501101a3e77804ac1ea21ee3c65a0349a4f34e00
Uzair Iqbal <uzair.qq@outlook.com>
2018-01-29 23:59:33 +0500

    generated new component called employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
+import { EmployeesComponent } from './employees/employees.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeesComponent,
   ],
   imports: [
     BrowserModule
