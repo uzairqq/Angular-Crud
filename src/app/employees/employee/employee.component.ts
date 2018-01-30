2409961dd8bbedde5e7d9cef49becb1762446a68
Uzair Iqbal <uzair.qq@outlook.com>
2018-01-31 00:22:30 +0500

    used employe models in employee servie and injected this employee service in employees , employee and employee-list and defined only in employees component service provider

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../shared/employee.model';
 
 @Component({
   selector: 'app-employee',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private employeeService: Employee) { }
+  
   ngOnInit() {
   }
 
--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Injectable()
 export class EmployeeService {
 
+  selectedEmployee: Employee;
   constructor() { }
 
 }
