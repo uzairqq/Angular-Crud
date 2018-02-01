46250aa26a6f73fe11b24cf1897e1a64271debb3
Uzair Iqbal <uzair.qq@outlook.com>
2018-02-01 19:20:00 +0500

    called employeList method in employee list and imported employee models

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../shared/employee.service';
 
+import {Employee} from '../../shared/employee.model';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -11,6 +12,7 @@ export class EmployeeListComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employeeService.getEmployeeList();
   }
 
 }
